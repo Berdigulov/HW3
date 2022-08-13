aa2fc81571961e834ea36dd53689131940e57a81
Berdigulov <berdigulovvv@gmail.com>
2022-08-13 14:17:27 +0300

    up2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String[] args) throws LimitException {
+    public static void main(String[] args){
         BankAccount bankAccount = new BankAccount();
         bankAccount.deposit(20000);
         while(true){
